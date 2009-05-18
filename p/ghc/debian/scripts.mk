542852082b6489840b0189e3071f17dcb7d67d11
Kari Pahula <kaol@debian.org>
2009-05-18 23:18:18 +0400

    ghc: Import of ghc6_6.10.3-1.dsc

--- p/ghc/debian/scripts.mk
+++ p/ghc/debian/scripts.mk
@@ -1,7 +1,8 @@
 #!/usr/bin/make -f
 
 FILES  = debian/ghc6.postinst debian/ghc6.prerm debian/ghc6.postrm
-FILES += debian/ghc6-doc.postinst
+FILES += debian/ghc6-doc.postinst debian/gen_contents_index debian/ghc6-doc.triggers
+FILES += debian/ghc6-doc.preinst
 
 .PHONY: all clean
 
