2884d3425742e175e70601fc8f40858df269d23a
Kari Pahula <kaol@debian.org>
2010-01-12 17:29:03 +0300

    ghc-jessie: Import of ghc6_6.12.1-1.dsc

--- p/ghc-jessie/debian/scripts.mk
+++ p/ghc-jessie/debian/scripts.mk
@@ -1,7 +1,8 @@
 #!/usr/bin/make -f
 
-FILES  = debian/ghc6.postinst debian/ghc6.prerm debian/ghc6.postrm
-FILES += debian/ghc6-doc.postinst
+FILES  = debian/ghc6.postinst debian/ghc6.prerm
+FILES += debian/ghc6-doc.postinst debian/gen_contents_index debian/ghc6-doc.triggers
+FILES += debian/ghc6-doc.preinst debian/ghc-pkg6.man
 
 .PHONY: all clean
 
