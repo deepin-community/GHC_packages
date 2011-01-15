ebd78fe8be4df40e5f774cd8a48b79d6bc3b2632
Joachim Breitner <mail@joachim-breitner.de>
2011-01-15 16:43:59 +0300

    ghc-jessie: Some more of s/ghc-pkg6/ghc-pkg

--- p/ghc-jessie/debian/scripts.mk
+++ p/ghc-jessie/debian/scripts.mk
@@ -2,7 +2,7 @@
 
 FILES  = debian/ghc.postinst debian/ghc.prerm
 FILES += debian/ghc-doc.postinst debian/gen_contents_index debian/ghc-doc.triggers
-FILES += debian/ghc-doc.preinst debian/ghc-pkg6.man
+FILES += debian/ghc-doc.preinst debian/ghc-pkg.man
 
 .PHONY: all clean
 
