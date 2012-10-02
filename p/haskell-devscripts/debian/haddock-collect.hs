d57d263a26633c1efaf796c8dc1d0e15e967b125
kiwamu <kiwamu@debian.or.jp>
2012-10-02 18:32:38 +0400

    haskell-devscripts: Not support gzip file and stripPrefix

--- p/haskell-devscripts/debian/haddock-collect.hs
+++ p/haskell-devscripts/debian/haddock-collect.hs
@@ -3,11 +3,12 @@
 
 import Data.List
 import Data.Char
+import Data.Maybe
 import System.Environment
 import System.FilePath
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy.Char8 as BL8
-import Codec.Compression.GZip
+--import qualified Data.ByteString.Lazy as BL
+--import qualified Data.ByteString.Lazy.Char8 as BL8
+--import Codec.Compression.GZip
 
 type URL = String
 
@@ -33,22 +34,22 @@ filterHaddock url = unlines . haddockHacks . haddockPackageUrl url . lines
 
 readFileGz :: FilePath -> IO String
 readFileGz file
-  | takeExtension file == ".gz" = do
-    c <- BL.readFile file
-    return $ BL8.unpack . decompress $ c
+--  | takeExtension file == ".gz" = do
+--    c <- BL.readFile file
+--    return $ BL8.unpack . decompress $ c
   | otherwise = readFile file
 
-readAndPrint :: URL -> FilePath -> FilePath -> IO ()
-readAndPrint prefix outdir file = do
+readAndPrint :: URL -> String -> FilePath -> FilePath -> IO ()
+readAndPrint prefix strip outdir file = do
   f <- readFileGz file
   writeFile outfile $ filterHaddock url f
     where dropgz f | takeExtension f == ".gz" = dropExtension f
                    | otherwise                = f
           outfile = outdir </> takeFileName (dropgz file)
-          url = prefix ++ dropFileName file
+          url = prefix ++ (fromMaybe "" . stripPrefix strip . dropFileName $ file)
 
 main :: IO ()
 main = do
-  [p, o] <- getArgs
+  [p, s, o] <- getArgs
   c <- getContents
-  mapM_ (readAndPrint p o) (lines c)
+  mapM_ (readAndPrint p s o) (lines c)
